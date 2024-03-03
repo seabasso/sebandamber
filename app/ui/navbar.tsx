13a91ba66943accca67b081b35a885f3ab16a5b9
Sebastian <seabassochoa@gmail.com>
2024-03-02 23:14:30 -0800

    Initial website

---
+++ app/ui/navbar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import styles from './navbar.module.css';
+import NavLinks from './navlinks';
+import { PowerIcon } from '@heroicons/react/24/outline';
+
+export default function NavBar() {
+    return (
+      <div className={styles.main}>
+        sebandamber.com :)
+      </div>
+    );
+  }
